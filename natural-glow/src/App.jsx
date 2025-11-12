016c91eb6e433c756fda7e14ee53b2ce2944a0eb
Evaline <evalinenkirote890@gmail.com>
2025-11-12 18:38:08 +0000

    added responsive Navigation bar with glow color branding"

--- natural-glow/src/App.jsx
+++ natural-glow/src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Header from "./components/layout/Header";
+import Navigation from "./components/layout/Navigation";
 import Hero from "./components/sections/Hero";
 import About from "./components/sections/About";
+import Features from "./components/sections/Features";
 
 function App() {
   return (
     <div className="relative">
       <Header />
+      <Navigation />
       <Hero />
       <About />
+      <Features/>
     </div>
   );
 }
